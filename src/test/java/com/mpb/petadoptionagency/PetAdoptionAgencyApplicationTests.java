0c835d90d931d911bd9501b6cfae9486c9b92618
MB <m.p.bialoszewski@outlook.com>
2026-01-25 13:39:37 +0000

    cleanup

--- src/test/java/com/mpb/petadoptionagency/PetAdoptionAgencyApplicationTests.java
+++ src/test/java/com/mpb/petadoptionagency/PetAdoptionAgencyApplicationTests.java
@@ -1,13 +1,13 @@
-package com.mpb.petadoptionagency;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PetAdoptionAgencyApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.mpb.petadoptionagency;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PetAdoptionAgencyApplicationTests {
+//
+////    @Test
+////    void contextLoads() {
+////    }
+//
+//}
