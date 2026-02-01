340762355ef2efa3acf32d0f14b3f530c87f15fb
MB <m.p.bialoszewski@outlook.com>
2026-02-01 12:56:09 +0000

    almost working hero banner

--- src/main/frontend/src/components/home/atoms/Hero.jsx
+++ src/main/frontend/src/components/home/atoms/Hero.jsx
@@ -1,6 +1,14 @@
 const Hero = () => (
     <div className="hero-container">
-        <h1>Hero</h1>
+        <div className="hero-content">
+            <div className="hero-title">
+                <h1>Dog Adoption Agency</h1>
+            </div>
+            <div className="hero-description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum enim in lectus congue, sed ornare lacus scelerisque. Aenean at ante ac lorem gravida rutrum vel quis felis. Suspendisse auctor, purus vitae convallis interdum, mi felis viverra nibh, ac facilisis ipsum erat vel enim. Nulla pretium tellus et pellentesque consectetur. Sed hendrerit nisi est. Maecenas nec.</p>
+            </div>
+            <button className="hero-button">Adopt now!</button>
+        </div>
     </div>
 )
 export default Hero;
