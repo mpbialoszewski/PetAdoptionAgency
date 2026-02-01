f44493c8f7962f43f156625077d9e571031c429b
MB <m.p.bialoszewski@outlook.com>
2026-02-01 12:56:20 +0000

    placeholder for services

--- src/main/frontend/src/components/home/atoms/Services.jsx
+++ src/main/frontend/src/components/home/atoms/Services.jsx
@@ -1,8 +1,23 @@
 const Services = () => {
     return(
-        <div className="services">
-            <h1>Services
-            </h1>
+        <div className="services-container">
+            <div className="services-content">
+                <div className="services-title">
+                    <h1>Services</h1>
+                </div>
+                <div className="services-description">
+                <div className="services-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum enim in lectus congue, sed ornare lacus scelerisque. Aenean at ante ac lorem gravida rutrum vel quis felis. Suspendisse auctor, purus vitae convallis interdum, mi felis viverra nibh, ac facilisis ipsum erat vel enim. Nulla pretium tellus et pellentesque consectetur. Sed hendrerit nisi est. Maecenas nec.</p>
+                </div>
+                <div className="services-list">
+                <ul>
+                    <li>A</li>
+                    <li>b</li>
+                    <li>c</li>
+                </ul>
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
