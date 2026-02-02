c15c32d1111dd20cb6bcb6e1bad29306c302cab2
MB <m.p.bialoszewski@outlook.com>
2026-02-02 21:09:29 +0000

    userRepository

---
+++ src/main/java/com/mpb/petadoptionagency/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.mpb.petadoptionagency.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository {
+    //TODO: Implement methods
+}
