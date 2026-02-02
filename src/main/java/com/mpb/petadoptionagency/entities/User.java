1e40f7f91e10b8c936285609567b00a592c61c02
MB <m.p.bialoszewski@outlook.com>
2026-02-02 21:49:53 +0000

    user entity

--- src/main/java/com/mpb/petadoptionagency/entities/User.java
+++ src/main/java/com/mpb/petadoptionagency/entities/User.java
@@ -1,9 +1,6 @@
 package com.mpb.petadoptionagency.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
@@ -14,10 +11,11 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity
+@Table(name="APP_USER")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
 
     @NotNull
@@ -27,21 +25,21 @@ public class User implements UserDetails {
     private String password;
 
     @NotNull
-    private String permission_level;
+    private String permissionLevel;
 
     public User(){
 
     }
 
-    public User(String username, String password, String permission_level) {
+    public User(String username, String password, String permissionLevel) {
         this.username = username;
         this.password = password;
-        this.permission_level = permission_level;
+        this.permissionLevel = permissionLevel;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("Role"+ this.permission_level));
+        return List.of(new SimpleGrantedAuthority("Role"+ this.permissionLevel));
     }
 
     @Override
@@ -52,5 +50,46 @@ public class User implements UserDetails {
     @Override
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    public String getPermissionLevel() {
+        return permissionLevel;
+    }
+
+    public void setPermissionLevel(String permission_level) {
+        this.permissionLevel = permission_level;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "role='" + permissionLevel + '\'' +
+                ", password='" + password + '\'' +
+                ", username='" + username + '\'' +
+                '}';
     }
 }
